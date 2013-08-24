4e46f1ae6e30aa2b7de30841356fd344cd9b54e5
Keppelen <keppelen.as@gmail.com>
2013-08-24 17:13:58 -0300

    finalizando mvp

---
+++ js/application.js
@@ -0,0 +1,35 @@
+function myFinance ( $scope ) {
+
+  if ( localStorage.getItem("myDados") == null ) {
+    $scope.dados = [];
+  }
+  else {
+    $scope.dados = [];
+    $scope.dados = JSON.parse( localStorage.getItem("myDados") );
+  }
+
+  $scope.valueTotalMoney = 200;
+  
+  
+  $scope.addTodo = function() {
+    $scope.dados.push( 
+      { 
+        description:$scope.TextDescription, valor: $scope.textValor 
+      });
+
+    localStorage.setItem( "myDados", JSON.stringify( $scope.dados ) );
+    $scope.TextDescription = '';
+    $scope.textValor = '';
+
+    var content = document.getElementById("content");
+    var footer = document.getElementById("footer");
+    var back = document.getElementById("back");
+
+    content.style.display = "block"
+    footer.style.display = "none"
+  };
+
+
+}
+
+
